46ddb04e117a6562851a9afc8e34a992a897f474
rcruz <rubenamcruz@gmail.com>
2020-08-13 14:32:59 +0100

    number insertion and deletion mecanism created

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './css/squares.css';
 class Square extends React.Component{
     render() {
         return (
-        <button className={this.getClassValue()}>
+        <button className={this.getClassValue()} onClick={() => this.props.onClick()}>
             {this.props.value}
         </button>
         )
@@ -60,30 +60,54 @@ class Board extends React.Component{
 
     constructor(props) {
         super(props);
+        let squares = Array(9).fill(null);
+        for(let i = 0; i < 9; i++){
+            squares[i] = Array(9).fill(null);
+        }
+
+        this.state = {
+          squares: squares,
+          selected: {line: null, column: null}
+        };
       }
 
-    renderSquare(line, column){
+    renderSquare(line, column, value){
         return(
-        <Square line={line} column={column}/>
+        <Square line={line} column={column} value={value} 
+            onClick={() => this.doTheThing(line-1, column-1)}/>
         )
+    }
+
+    doTheThing(line, column){
+        this.setState({selected: {line: line, column: column}});
     }
 
     renderLine(line_number){
         return (
           <div className="board-row">
-            {this.renderSquare(line_number, 1)}
-            {this.renderSquare(line_number, 2)}
-            {this.renderSquare(line_number, 3)}
-            {this.renderSquare(line_number, 4)}
-            {this.renderSquare(line_number, 5)}
-            {this.renderSquare(line_number, 6)}
-            {this.renderSquare(line_number, 7)}
-            {this.renderSquare(line_number, 8)}
-            {this.renderSquare(line_number, 9)}
+            {this.renderSquare(line_number, 1, this.state.squares[line_number-1][0])}
+            {this.renderSquare(line_number, 2, this.state.squares[line_number-1][1])}
+            {this.renderSquare(line_number, 3, this.state.squares[line_number-1][2])}
+            {this.renderSquare(line_number, 4, this.state.squares[line_number-1][3])}
+            {this.renderSquare(line_number, 5, this.state.squares[line_number-1][4])}
+            {this.renderSquare(line_number, 6, this.state.squares[line_number-1][5])}
+            {this.renderSquare(line_number, 7, this.state.squares[line_number-1][6])}
+            {this.renderSquare(line_number, 8, this.state.squares[line_number-1][7])}
+            {this.renderSquare(line_number, 9, this.state.squares[line_number-1][8])}
         </div>
         )
     }
 
+
+    fillTheValues(key){
+        let squares = this.state.squares.slice();
+        if(key === 'Delete'){
+            squares[this.state.selected.line][this.state.selected.column] = null;
+        }else if(key >= '0' && key <= '9'){
+            squares[this.state.selected.line][this.state.selected.column] = Number(key);
+        }
+        this.setState({squares: squares});
+    }
 
     render(){
         return (
